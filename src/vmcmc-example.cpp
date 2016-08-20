7a7ac44f3850f422e9c45d25b60a0e86680d0e6d
mkleesiek <marco@kleesiek.com>
2016-08-21 00:26:12 +0200

    updated Doxygen configuration

--- src/vmcmc-example.cpp
+++ src/vmcmc-example.cpp
@@ -16,11 +16,29 @@ LOG_DEFINE("vmcmc.example")
 using namespace std;
 using namespace vmcmc;
 
+/**
+ * A standard normal (Gaussian) distribution.
+ * @param x the function argument
+ * @param mean the mean of the distribution.
+ * @param sigma the standard deviation.
+ * @return
+ */
 double normalPdf(double x, double mean, double sigma)
 {
     return 1.0 / (constants::root_two_pi * sigma) * exp( - 0.5 * math::pow<2>((x - mean) / sigma) );
 }
 
+/**
+ * A 2-dimensional normal distribution.
+ * @param x1 function argument #1
+ * @param x2 function argument #2
+ * @param mean1 distribution mean in dim. 1
+ * @param mean2 distribution mean in dim. 2
+ * @param sigma1 standard deviation in dim. 1
+ * @param sigma2 standard deviation in dim. 2
+ * @param corr correlation between x1 and x2
+ * @return
+ */
 double biVariateNormalPdf(double x1, double x2, double mean1, double mean2, double sigma1, double sigma2, double corr)
 {
     if (sigma1 == 0.0)
@@ -33,6 +51,12 @@ double biVariateNormalPdf(double x1, double x2, double mean1, double mean2, doub
     return 1.0 / (constants::two_pi*sigma1*sigma2*std::sqrt(1.0-corr*corr)) * std::exp(-z/(2.0*(1.0-corr*corr)));
 }
 
+/**
+ * The likelihood function (a bivariate normal distribution) for this particular
+ * example run.
+ * @param params The first two vector elements are used as function parameters.
+ * @return
+ */
 double targetFunction(const vector<double>& params) {
     const double x1 = params[0];
     const double x2 = params[1];
@@ -41,23 +65,30 @@ double targetFunction(const vector<double>& params) {
 
 int main(int /*argc*/, char* /*argv*/[]){
 
+    // choose a non-deterministic seed for random number generator:
     Random::Instance().SetSeed(0);
 
+    // setup the parameter configuration
     ParameterList paramConfig;
     paramConfig.SetParameter(0, Parameter("x1", 0.0, 1.0) );
     paramConfig.SetParameter(1, Parameter("x2", 0.0, 1.0) );
     paramConfig.SetErrorScaling( 5.0 );
 
+    // instantiate the MCMC sampler
     MetropolisHastings mcmc;
     mcmc.SetParameterConfig(paramConfig);
     mcmc.SetRandomizeStartPoint(true);
     mcmc.SetBetas( {1.0, 0.1, 0.01, 0.001} );
     mcmc.SetLikelihoodFunction( targetFunction );
-    mcmc.SetTotalLength(1E5);
+    mcmc.SetTotalLength(1E7);
 
     LOG(Info, "Starting example Metropolis ...");
 
+    // execute
     mcmc.Run();
+
+    // TODO: print diagnostics
+    // TODO: very important: save the sampled chain as ASCII file
 
     LOG(Info, "Done.");
 
