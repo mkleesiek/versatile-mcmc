7d4f04c6e50fbb01d2fe531219b89d0880af7d15
Marco Kleesiek <marco.kleesiek@kit.edu>
2016-09-12 23:05:18 +0200

    doxygen update

--- search/all_1.js
+++ search/all_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['bivariatenormalpdf',['biVariateNormalPDF',['../math_8cpp.html#aa3ee89240f435a5c2e5789d68e23576c',1,'vmcmc::math']]],
+  ['bivariatenormalpdf',['biVariateNormalPDF',['../math_8cpp.html#a7418a17ba33d3eae6eedbcb8dd09fcdd',1,'vmcmc::math']]],
   ['blas_2ehpp',['blas.hpp',['../blas_8hpp.html',1,'']]],
   ['bool',['Bool',['../classvmcmc_1_1RandomPrototype.html#a9b5105263dee77a613b8af81ab5ab341',1,'vmcmc::RandomPrototype']]]
 ];
